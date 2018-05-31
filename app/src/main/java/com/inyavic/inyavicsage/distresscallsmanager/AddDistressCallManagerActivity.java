a8c8c40213162a25439869cb896430fb1847d1de
Inyavic Sage <inyavicsage@gmail.com>
2018-05-31 17:31:05 +0100

    initial commit

---
+++ app/src/main/java/com/inyavic/inyavicsage/distresscallsmanager/AddDistressCallManagerActivity.java
@@ -0,0 +1,13 @@
+package com.inyavic.inyavicsage.distresscallsmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddDistressCallManagerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_distress_call_manager);
+    }
+}
